a8ff11c58f06ff9b1e9536f0651d0ee6f801da6b
OstapSpas <74359369+OstapSpas@users.noreply.github.com>
2023-05-31 16:12:03 +0300

    Add files via upload

---
+++ HW-29.05/app.js
@@ -0,0 +1,90 @@
+
+
+//Task 1 1. Сума елементів
+
+// function arrSum(num){
+//     let res = 0;
+//     for(let i = 0; i < num.length; i++){
+//         res += num[i];
+//     }
+//     return res;
+// }
+
+// console.log(arrSum([22, 4, 3.3]));
+
+// Task 2 Більше нуля
+
+// function arrSum(nums) {
+//     let arr = [];
+//     for(let i = 0; i < nums.length; i++ ){
+//         if(nums[i] > 0){
+//             arr.push(nums[i]);
+//         }
+//     }
+//     return arr;
+// }
+// console.log(arrSum([22, -4, 3.3])); 
+
+// Менше середнього
+
+// function arrSum(nums) {
+//     let res = 0;
+//     for(let i = 0; i< nums.length; i++){
+//         res += nums[i];
+        
+//     }
+//     let s =res/nums.length;
+
+//     let arr = [];
+//     for(let j = 0; j < nums.length; j++ ){
+//         if(nums[j] < s){
+//             arr.push(nums[j]);
+//         }
+//     }
+
+//     return arr;    
+// }
+
+// console.log(arrSum([22, 4, 3.3])); 
+
+// Сортування
+
+
+// function sortArr(numbers) {
+//     // Використовуємо рядок-компаратор для сортування
+//     numbers.sort((a, b) => a - b);
+  
+//     // Повертаємо відсортований масив
+//     return numbers;
+//   }
+
+//   const numbers = [8, 21, 576, 322, 4];
+// const sortedArray = sortArr(numbers);
+// console.log(sortedArray); // Виведе [1, 2, 3, 4, 5]
+
+// function sortArr(numbers) {
+//     numbers.sort((a, b) => b - a);
+  
+//     return numbers;
+//   }
+
+// const numbers = [8,233, 21, 576, 322, 4, 77,88,1000];
+// const sortedArray = sortArr(numbers);
+// console.log(sortedArray); 
+
+
+// Медіана
+
+function arrMed(nums){
+    let result = [];
+    for(let i = 0; i < result.length; i++){
+        if(nums.length % 2){
+            
+        }
+        else{
+             
+        }
+    }
+}
+
+console.log(arrMed());
